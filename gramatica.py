e7ca0ad6e73e4608bbccb67008d8e54c9d7e37fa
unknown <andrei.aldea@andreialdea.infotrend.local>
2014-05-07 12:21:52 +0300

    Mai e putin

--- gramatica.py
+++ gramatica.py
@@ -1,4 +1,11 @@
 from itertools import *
+import copy
+
+def unq(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if x not in seen and not seen_add(x)]
+
 class Symbol:
 	nume=None
 	def __init__(self,nume):
@@ -7,6 +14,8 @@ class Symbol:
 		return self.nume
 	def __eq__(self,other):
 		return self.nume==other
+	def __repr__(self):
+		return self.__str__()
 
 class Rule:
 	pleacaDin=None
@@ -20,6 +29,14 @@ class Rule:
 		for x in self.pleacaIn:
 			s=s+x.nume
 		return s
+	def __repr__(self):
+		return self.__str__() 
+	def nrOf(self,symbol):
+		c=0
+		for s in self.pleacaIn:
+			if symbol==s:
+				c=c+1
+		return c
 
 f=open("gramatica.txt",'r')
 gramatica=[]
@@ -36,9 +53,78 @@ for line in f:
 for x in gramatica:
 	print x
 
+delGram=[]
+newGram=[]
+done=False
+
+while not done:
+	print "------------------------START----------------------------"
+	done=True
+	for x in gramatica:
+		if '*' in x.pleacaIn:
+			done=False
+			for z in gramatica:
+				if x.pleacaDin in z.pleacaIn:
+					a=z.nrOf(x.pleacaDin)
+					comb=list(product([0,1],repeat=a))
+					for co in comb:
+						i=0
+						d=0
+						temp=copy.deepcopy(z.pleacaIn)
+						print "Temp:",temp,"Comb:",co
+						for s in z.pleacaIn:
+							if s==x.pleacaDin:
+								if co[i]==0:
+									print "Deleted:",temp[d]
+									del temp[d]
+									d=d-1
+								i=i+1
+							d=d+1
+						print "Temp final:",temp
+						if not temp:
+							temp=[Symbol('*'),]
+						newGram.append(Rule(z.pleacaDin,temp))
+						delGram.append(z)
+						delGram.append(x)
+						delGram=unq(delGram)
+					print x.pleacaDin,">>>>>",z,a,comb
+	gramatica=gramatica+newGram
+	gramatica=[i for i in gramatica if i not in delGram]
+	gramatica=unq(gramatica)
+	print "-------------------------END--------------------------------"
+
+print "Gramatica partea 1:",gramatica
+print "DelGram:",delGram
+delGram=[]
 for x in gramatica:
-	if '*' in x.pleacaIn:
-		for z in gramatica:
-			if x.pleacaDin in z.pleacaIn:
-				print ">>>>>",z
+	if len(x.pleacaIn)==1 and not x.pleacaIn[0]=='*' and x.pleacaIn[0].nume.isupper():
+		delGram.append(x)
+		for k in gramatica:
+			if k.pleacaDin==x.pleacaIn[0]:
+				gramatica.append(Rule(x.pleacaDin,k.pleacaIn))
+		
+
+	gramatica=[i for i in gramatica if i not in delGram]
+	gramatica=unq(gramatica)
+
+print "Gramatica partea 2:",gramatica
+
+surplus=1
+for x in gramatica:
+	if len(x.pleacaIn)==3:
+		newStr="V"+str(surplus)
+		newSymb=Symbol(newStr)
+		newRule=Rule(newStr,[x.pleacaIn[0],x.pleacaIn[1]])
+		surplus=surplus+1
+		x.pleacaIn=[newSymb,x.pleacaIn[2]]
+		gramatica.append(newRule)
+
+for x in gramatica:
+	for s in x.pleacaIn:
+		if s.nume.islower():
+			gramatica.append(Rule(s.nume.upper(),[Symbol(s.nume.upper()),]))
+			s.nume=s.nume.upper()
+
+gramatica=unq(gramatica)
+print "Gramatica finala:",gramatica
 
